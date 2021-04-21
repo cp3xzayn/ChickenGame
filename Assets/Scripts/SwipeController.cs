bb1c99418d1990b301cbb2b16646b405004ec9e9
cp3xzayn <cp3xzayn@gmail.com>
2021-04-21 20:22:37 +0900

    スマホの左半分で移動、右半分で視点変更を実装した。（両方同時にできる）

--- Assets/Scripts/SwipeController.cs
+++ Assets/Scripts/SwipeController.cs
@@ -3,19 +3,118 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// PCでもスマホでも使用できる1人称視点のカメラ移動
+/// PCでもスマホでも使用できる1人称視点のカメラ移動（スマホは視点移動＋プレイヤーの移動）
 /// </summary>
 public class SwipeController : MonoBehaviour
 {
-    /// <summary> Swipeしたときの回転スピード </summary>
-    [SerializeField] float m_swipeTurnSpeed = 0.01f;
+    /// <summary> スマホをSwipeしたときの回転スピード </summary>
+    [SerializeField] float m_swipeTurnSpeed = 0.1f;
+    /// <summary> 視点移動のモード </summary>
+    [SerializeField] SwipeMode m_swipeMode = SwipeMode.mouse;
+    /// <summary> Playerの移動速度 </summary>
+    float m_moveSpeed = 2f;
+    Rigidbody m_rb;
 
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        switch (m_swipeMode)
+        {
+            case SwipeMode.mouse:
+                MouseSwipe();
+                break;
+            case SwipeMode.phone:
+                TouchSwipe();
+                break;
+        }
+    }
+
+
+    // ↓スマホでの視点操作+Playerの移動
+
+    /// <summary>
+    /// スマホ画面のタッチでの視点移動+Playerの移動
+    /// </summary>
+    void TouchSwipe()
+    {
+        Touch touch;
+        // タッチされた指の数
+        int touchCount = Input.touchCount;
+
+        if (Input.touchCount == 1)
+        {
+            touch = Input.GetTouch(0);
+            BehaviorFromTouch(touch);
+        }
+        if (Input.touchCount == 2)
+        {
+            for (int i = 0; i < touchCount; i++)
+            {
+                touch = Input.GetTouch(i);
+                BehaviorFromTouch(touch);
+            }
+        }
+    }
+
+    /// <summary> 最初にタッチされた座標 </summary>
+    Vector3 startTouchPos;
+
+    /// <summary>
+    /// タッチされた場所が画面を半分にしたときの右か左かを判定する
+    /// 左：プレイヤーの移動　右：視点移動
+    /// </summary>
+    /// <param name="touch"></param>
+    void BehaviorFromTouch(Touch touch)
+    {
+        if (touch.position.x >= Screen.width / 2)
+        {
+            float x = touch.deltaPosition.x; // 偏差分を求める
+            // 左右に視点変更する時の角度
+            float angleY = this.transform.eulerAngles.y + x * m_swipeTurnSpeed;
+            // それぞれの角度をセットする
+            //今回はY方向の視点移動を使用しないため、xも0になっている
+            this.transform.eulerAngles = new Vector3(0, angleY, 0);
+        }
+        if (touch.position.x < Screen.width / 2)
+        {
+            // タッチされたとき
+            if (touch.phase == TouchPhase.Began)
+            {
+                // 初期化する
+                startTouchPos = touch.position;
+            }
+            // タッチが継続的に続いているとき
+            if (touch.phase == TouchPhase.Stationary)
+            {
+                Vector3 nowTouchPos = touch.position; // 現在のタッチしている座標
+                // 差分を単位ベクトルにする
+                Vector3 diffPosNormalized = (nowTouchPos - startTouchPos).normalized; 
+                // プレイヤーを移動する
+                m_rb.velocity = transform.forward * diffPosNormalized.y * m_moveSpeed;
+            }
+
+        }
+    }
+
+
+
+    // ↓マウスでの操作（視点操作のみ）
+    
+    /// <summary> マウスをSwipeしたときの回転スピード </summary>
+    [SerializeField] float m_mouseTurnSpeed = 0.01f;
     /// <summary> タッチされた初期位置 </summary>
     Vector3 m_mousePos;
     /// <summary> 画面の右側が選択されたかを判定する </summary>
     bool isRightOnScreen = false;
 
-    void Update()
+    /// <summary>
+    /// マウスでのスワイプに関する処理
+    /// </summary>
+    void MouseSwipe()
     {
         // マウスが押されたタイミングで
         if (Input.GetMouseButtonDown(0))
@@ -39,14 +138,23 @@ public class SwipeController : MonoBehaviour
                 Vector3 diffMousePos = m_mousePos - nextMousePos;
 
                 // 上下視点の角度(制限をつける)
-                float angleX = this.transform.eulerAngles.x + diffMousePos.y * m_swipeTurnSpeed;
+                float angleX = this.transform.eulerAngles.x + diffMousePos.y * m_mouseTurnSpeed;
 
                 // 左右視点の角度
-                float angleY = this.transform.eulerAngles.y + diffMousePos.x * m_swipeTurnSpeed;
+                float angleY = this.transform.eulerAngles.y + diffMousePos.x * m_mouseTurnSpeed;
 
                 // それぞれの角度をセットする
                 this.transform.eulerAngles = new Vector3(0, angleY, 0); //今回はY方向の視点移動を使用しないため、xも0になっている
             }
         }
     }
+}
+
+/// <summary>
+/// 視点移動の操作モード(PC, スマホ)
+/// </summary>
+public enum SwipeMode
+{
+    mouse,
+    phone
 }
