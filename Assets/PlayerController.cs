b6531219f8716ac333fb629f0a104f9c8be608af
cp3xzayn <cp3xzayn@gmail.com>
2021-04-21 12:26:58 +0900

    初回コミット

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary> 戦車のスピード </summary>
+    [SerializeField] float m_speed = 2;
+    /// <summary> JoyStick </summary>
+    [SerializeField] FloatingJoystick m_floatingJoystick = null;
+    Rigidbody m_rb;
+    /// <summary> 入力された方向ベクトル </summary>
+    private Vector3 m_direction;
+    /// <summary> 入力された方向ベクトル </summary>
+    public Vector3 Direction => m_direction;
+
+    void Start()
+    {
+        m_rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (m_direction != Vector3.zero)
+        {
+            transform.localRotation = Quaternion.LookRotation(m_direction);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        m_direction = Vector3.forward * m_floatingJoystick.Vertical + Vector3.right * m_floatingJoystick.Horizontal;
+        m_rb.velocity = m_direction * m_speed;
+    }
+}
---
+++ Assets/SwipeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// PCでもスマホでも使用できる3人称視点のカメラ移動
+/// </summary>
+public class SwipeController : MonoBehaviour
+{
+    /// <summary> Swipeしたときの回転スピード </summary>
+    [SerializeField] float m_swipeTurnSpeed = 30f;
+    /// <summary> タッチされた初期位置 </summary>
+    Vector3 m_mousePos;
+
+    void Update()
+    {
+        // マウスが押されたタイミングで
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 初期位置を初期化する
+            m_mousePos = Input.mousePosition; 
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 nextMousePos = Input.mousePosition;
+            Vector3 diffMousePos = m_mousePos - nextMousePos;
+
+            float angleX = this.transform.eulerAngles.x - diffMousePos.y * m_swipeTurnSpeed * 0.01f;
+            float angleY = this.transform.eulerAngles.y + diffMousePos.x * m_swipeTurnSpeed * 0.01f;
+
+            this.transform.eulerAngles = new Vector3(angleX, angleY, 0);
+        }
+    }
+}
