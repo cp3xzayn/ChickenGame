c360c6788244d5201f71c9c3d5824cc8c3f0066e
cp3xzayn <cp3xzayn@gmail.com>
2021-04-21 14:58:07 +0900

    画面の右側をスワイプしたときのみ、視点変更を行うようにした。

--- Assets/Scripts/SwipeController.cs
+++ Assets/Scripts/SwipeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// PCでもスマホでも使用できる3人称視点のカメラ移動
+/// PCでもスマホでも使用できる1人称視点のカメラ移動
 /// </summary>
 public class SwipeController : MonoBehaviour
 {
     /// <summary> Swipeしたときの回転スピード </summary>
-    [SerializeField] float m_swipeTurnSpeed = 30f;
+    [SerializeField] float m_swipeTurnSpeed = 0.01f;
+
     /// <summary> タッチされた初期位置 </summary>
     Vector3 m_mousePos;
+    /// <summary> 画面の右側が選択されたかを判定する </summary>
+    bool isRightOnScreen = false;
 
     void Update()
     {
@@ -18,17 +21,32 @@ public class SwipeController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             // 初期位置を初期化する
-            m_mousePos = Input.mousePosition; 
+            m_mousePos = Input.mousePosition;
+            if (m_mousePos.x >= Screen.width / 2)
+            {
+                isRightOnScreen = true;
+            }
+            else
+            {
+                isRightOnScreen = false;
+            }
         }
         if (Input.GetMouseButton(0))
         {
-            Vector3 nextMousePos = Input.mousePosition;
-            Vector3 diffMousePos = m_mousePos - nextMousePos;
+            if (isRightOnScreen)
+            {
+                Vector3 nextMousePos = Input.mousePosition;
+                Vector3 diffMousePos = m_mousePos - nextMousePos;
 
-            float angleX = this.transform.eulerAngles.x - diffMousePos.y * m_swipeTurnSpeed * 0.01f;
-            float angleY = this.transform.eulerAngles.y + diffMousePos.x * m_swipeTurnSpeed * 0.01f;
+                // 上下視点の角度(制限をつける)
+                float angleX = this.transform.eulerAngles.x + diffMousePos.y * m_swipeTurnSpeed;
 
-            this.transform.eulerAngles = new Vector3(angleX, angleY, 0);
+                // 左右視点の角度
+                float angleY = this.transform.eulerAngles.y + diffMousePos.x * m_swipeTurnSpeed;
+
+                // それぞれの角度をセットする
+                this.transform.eulerAngles = new Vector3(angleX, angleY, 0);
+            }
         }
     }
 }
