e6a03b7a5bec59ed607992dd87d6356c790784b6
cp3xzayn <cp3xzayn@gmail.com>
2021-04-29 19:08:37 +0900

    回転するオブジェクトを作成した。またPlay中しか回転しないようにした。

---
+++ Assets/Scripts/RotateCylinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCylinder : MonoBehaviour
+{
+    [SerializeField] float m_turnSpeed = 3f;
+
+    void Update()
+    {
+        if (GameManager.Instance.NowGameState == GameState.Playing)
+        {
+            this.gameObject.transform.Rotate(new Vector3(1, 0, 0) * m_turnSpeed);
+        }
+    }
+}
