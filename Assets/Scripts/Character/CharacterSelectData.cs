7bfb7f3d5d7eac717ee3b10ccef1cdffdbd136fe
cp3xzayn <cp3xzayn@gmail.com>
2021-05-02 16:17:17 +0900

    他のPlayerを待つ、待機画面を作成した。またキャラ選択画面を作成した。

---
+++ Assets/Scripts/Character/CharacterSelectData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CharaData", menuName = "ScriptableObjects/CreateCharaDataAsset")]
+public class CharacterSelectData : ScriptableObject
+{
+    public List<CharacterParam> m_charaParamList = new List<CharacterParam>();
+}
+
+[System.Serializable]
+public class CharacterParam
+{
+    /// <summary> キャラの名前 </summary>
+    [Header("キャラの名前")] public string m_name;
+    /// <summary> キャラの画像 </summary>
+    [Header("キャラの画像")] public Sprite m_image;
+}
+
---
+++ Assets/Scripts/Character/SelectCharaInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharaInfo
+{
+    static string m_charaName;
+
+    public static string CharaName
+    {
+        set { m_charaName = value; }
+        get { return m_charaName; }
+    }
+}
